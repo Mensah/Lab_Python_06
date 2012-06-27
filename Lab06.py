cc190cafb27aac35204e16d2f1081f78c1dd8458
Mensah Adjei <nbadjei@gmail.com>
2012-06-27 15:16:14 +0000

    Lab06

--- Lab06.py
+++ Lab06.py
@@ -1,4 +1,4 @@
-import datetime
+
 class Player:
     def __init__(self,firstname,lastname,team=None):
         self.first_name = firstname
@@ -7,27 +7,27 @@ class Player:
         self.team = team
 
     def add_score(self,score):
-        scoree = self.scores.append(score)
-        return scoree
+        self.scores.append(score)
 
     def total_score(self):
-        t_score = sum(self.scores)
-        return t_score
+        t_score = (self.scores)
+        return sum(t_score)
 
     def average_score(self):
-        average = total_score/len(self.scores)
+        average = sum(self.scores)/float(len(self.scores))
         return average
 
     def __str__(self):
         
-       # description = 'Senior ' + self.first_name + ' ' + self.last_name + ', the ' + self.team
+        description = 'Senior ' + self.first_name + ' ' + self.last_name + ', from ' + self.team
         return description
 
-    
-torres = Player('Fernando', 'Torres', 'Espanyol')
+'''    
+torres = Player('Fernando', 'Torres', Spain)
 #print torres
+spain = Player()
 
-goals = int(raw_input('Please enter the number of scorec you want to input: '))
+goals = int(raw_input('Please enter the number of scores you want to input: '))
 print 'Please input them: '
 for i in range(goals):
     s = int(raw_input())
@@ -36,7 +36,36 @@ for i in range(goals):
 
 #torres.average_score()
 print torres.scores
-print torres.total_score
+print torres.total_score()
+print torres.average_score()
+'''
+
+class Team:
+    def __init__(self,name,league,manager_name,points):
+        self.name = name
+        self.league = league
+        self.manager_name = manager_name
+        self.points = points
+        self.players = []
+
+    def add_player(self,player):
+        self.players.append(player)
+
+    def __str__(self):
+        description ='The ' + self.name + ' team currently managed by  ' + self.manager_name + ', are at the top of their group table with ' + self.points + 'points in the ' + self.league + ' Cup'
+        return description
+        
+        
+Portugal = Team('Portugal', 'Euro2012','Boye Ferguson', '4')
+Spain = Team('Espanyol', 'Euro2012','Nii Guardiola','6')
+torres = Player('Fernando', 'Torres', Spain)
+ronaldo = Player('Ronaldo', 'Cristiano', Portugal)
+Spain.add_player(torres)
+
+print Spain.players
+
+
+
 
 
 
--- Lab06_part3.py
+++ Lab06_part3.py
@@ -7,9 +7,13 @@ class Team:
         self.players = []
 
     def add_player(self,player):
-        playerr = self.players.append(player)
-        return playerr
+        self.players.append(player)
 
     def __str__(self):
-        description = 
+        description ='The ' + self.name + ' team currently managed by  ' + self.manager_name + ', are at the top of their group table with ' + self.points + 'points in the ' + self.league + ' Cup'
+        return description
         
+        
+
+Spain = Team('Espanyol', 'Euro2012','Nii Guardiola','6')
+print Spain
